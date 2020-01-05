f75724e5b867ef48a452bb13cd4b36f4cf5dfadd
Mikael Brudfors <brudfors@gmail.com>
2020-01-05 15:06:46 +0100

    Default: sett.var.v_settings = [1e-4 0 0.2 0.05 0.2]*2^3;

--- spm_mb_param.m
+++ spm_mb_param.m
@@ -339,9 +339,9 @@ if ~isfield(sett.var,'mu_settings')
 end
 if ~isfield(sett.var,'v_settings')
     if sett.do.updt_aff
-        sett.var.v_settings = [0 0 0.2 0.05 0.2]*2^4;
+        sett.var.v_settings = [0 0 0.2 0.05 0.2]*2^3;
     else
-        sett.var.v_settings = [1e-4 0 0.2 0.05 0.2]*2^4;
+        sett.var.v_settings = [1e-4 0 0.2 0.05 0.2]*2^3;
     end
 end
 %------------------
