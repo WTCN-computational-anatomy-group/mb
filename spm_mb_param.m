8d6e124c30561537b8becdd3a8e197635d104afc
Yael Balbastre <y.balbastre@ucl.ac.uk>
2020-01-08 19:58:52 +0000

    Doc

--- spm_mb_param.m
+++ spm_mb_param.m
@@ -135,7 +135,7 @@ if ~s.do.pca, s.do.updt_subspace     = false;
 s = SetDefault(s, 'gen.accel',    0.8); % 0 <= accel <= 1: 0 -> slow & stable; 1 -> fast & unstable                                       
                                         % If accel > 0, the Hessian is based on a mixture of 
                                         % the Bohning bound (stable) and the Taylor expansion (unstable)
-s = SetDefault(s, 'gen.run2d',    0);   % 0, 1, 2, 3 ?
+s = SetDefault(s, 'gen.run2d',    0);   % 0 = 3D | 1, 2, 3 = 2D slice along n-th dimension?
 s = SetDefault(s, 'gen.samp',     3);   % Sampling distance (mm)
 s = SetDefault(s, 'gen.max_mem',  1);   % Size (GB) above which the template/subspace is stored on disk
 % TODO: Maybe accel should move to .optim?
